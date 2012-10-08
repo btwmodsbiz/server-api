8d01dc96f14d2e218650da1fcb453bc369a0e473
Andre Mekkawi <github@andremekkawi.com>
2012-10-08 06:27:10 -0400

    Added missing code to IBlockListener.

--- src/btwmods/api/world/listeners/IBlockListener.java
+++ src/btwmods/api/world/listeners/IBlockListener.java
@@ -1,5 +1,8 @@
 package btwmods.api.world.listeners;
 
-public interface IBlockListener {
+import net.minecraft.src.Block;
+import net.minecraft.src.TileEntity;
 
+public interface IBlockListener {
+	public void blockContainerBroken(Block block, int blockMetadata, TileEntity tileEntity, int x, int y, int z);
 }
