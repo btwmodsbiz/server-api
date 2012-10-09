c0f80f5c961b285825241e29213d89dc3535218f
Andre Mekkawi <github@andremekkawi.com>
2012-10-09 10:34:50 -0400

    WIP changes to ItemLogger.

--- src/btwmods/mods/itemlogger/ItemLogger.java
+++ src/btwmods/mods/itemlogger/ItemLogger.java
@@ -1,6 +1,11 @@
 package btwmods.mods.itemlogger;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Logger;
+
+import btwmods.BasicFormatter;
 
 public class ItemLogger {
 	private static Logger logger = null;
@@ -8,6 +13,19 @@ public class ItemLogger {
 	public static Logger GetLogger() {
 		if (logger == null) {
 			logger = Logger.getLogger("btwmods.mods.ItemLogger");
+			
+			try {
+				FileHandler handler = new FileHandler(new File(".", "itemlogger.%g.log").getPath(), 10 * 1024, 100);
+				handler.setFormatter(new BasicFormatter());
+				logger.addHandler(handler);
+				
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		return logger;
