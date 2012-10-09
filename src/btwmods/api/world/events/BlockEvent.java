a333b9af9e09084dcd4df9bc43993eaf9cb2127c
Andre Mekkawi <github@andremekkawi.com>
2012-10-08 21:18:01 -0400

    BlockEvent can now get the contents of the block (if it is an IInventory).

--- src/btwmods/api/world/events/BlockEvent.java
+++ src/btwmods/api/world/events/BlockEvent.java
@@ -3,6 +3,8 @@ package btwmods.api.world.events;
 import java.util.EventObject;
 
 import net.minecraft.src.Block;
+import net.minecraft.src.IInventory;
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.TileEntity;
 import btwmods.api.world.WorldAPI;
 
@@ -13,11 +15,14 @@ public class BlockEvent extends EventObject {
 	private TYPE type;
 	private WorldAPI api;
 	private Block block;
+	private int metadata;
+	private int x;
+	private int y;
+	private int z;
+	
+	private boolean checkedTileEntity = false;
 	private TileEntity tileEntity = null;
-	private int metadata = -1;
-	private int x = -1;
-	private int y = -1;
-	private int z = -1;
+	private ItemStack[] contents = null;
 	
 	public TYPE getType() {
 		return type;
@@ -32,10 +37,29 @@ public class BlockEvent extends EventObject {
 	}
 	
 	public TileEntity getTileEntity() {
-		if (tileEntity == null)
-			tileEntity = api.world.getBlockTileEntity(getX(), getY(), getZ());
+		if (!checkedTileEntity) {
+			tileEntity = api.world.getBlockTileEntity(x, y, z);
+			checkedTileEntity = true;
+		}
 		
 		return tileEntity;
+	}
+	
+	public boolean hasInventory() {
+		return contents != null || getTileEntity() instanceof IInventory;
+	}
+	
+	public ItemStack[] getContents() {
+		if (contents == null && hasInventory()) {
+			IInventory inventory = (IInventory)getTileEntity();
+			
+			contents = new ItemStack[inventory.getSizeInventory()];
+			for (int i = 0; i < contents.length; i++) {
+				contents[i] = inventory.getStackInSlot(i);
+			}
+		}
+		
+		return contents;
 	}
 	
 	public int getMetadata() {
