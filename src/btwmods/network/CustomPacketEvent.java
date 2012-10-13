0e4133f270f991a72d4a1b810072e60c9bd5e2d7
Andre Mekkawi <github@andremekkawi.com>
2012-10-12 20:58:08 -0400

    Added missing accessors to CustomPacketEvent.

--- src/btwmods/network/CustomPacketEvent.java
+++ src/btwmods/network/CustomPacketEvent.java
@@ -7,6 +7,27 @@ public class CustomPacketEvent extends EventObject {
 	private String channel;
 	private byte[] data;
 	private int length;
+	private boolean isHandled = true;
+	
+	public String getChannel() {
+		return channel;
+	}
+	
+	public byte[] getData() {
+		return data;
+	}
+	
+	public int getLength() {
+		return length;
+	}
+	
+	public boolean isHandled() {
+		return isHandled;
+	}
+	
+	public void setNotHandled() {
+		isHandled = false;
+	}
 	
 	public CustomPacketEvent(String channel, byte[] data, int length) {
 		super(channel);
