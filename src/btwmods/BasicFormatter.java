685ccedc69c7d8e29f902873a45e36881c7c2cba
Andre Mekkawi <github@andremekkawi.com>
2012-10-13 20:44:37 -0400

    Changed the BasicFormatter's dateFormat to public and final.

--- src/btwmods/BasicFormatter.java
+++ src/btwmods/BasicFormatter.java
@@ -6,7 +6,7 @@ import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
 public class BasicFormatter extends Formatter {
-	private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss Z");
+	public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss Z");
 
 	@Override
 	public String format(LogRecord record) {
