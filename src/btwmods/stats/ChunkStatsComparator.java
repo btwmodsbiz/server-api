77505d43ea3be78a1a7879e3525ba9c6d436ee43
Andre Mekkawi <github@andremekkawi.com>
2012-10-15 09:44:57 -0400

    Added comparator for sorting ChunkStats.

---
+++ src/btwmods/stats/ChunkStatsComparator.java
@@ -0,0 +1,34 @@
+package btwmods.stats;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import btwmods.StatsAPI.StatsProcessor.ChunkStats;
+
+public class ChunkStatsComparator<T> implements Comparator<Map.Entry<T, ChunkStats>> {
+	
+	public enum Stat { TICKTIME, ENTITIES };
+	public final int alternator;
+	public final Stat stat;
+	
+	public ChunkStatsComparator(Stat stat) {
+		this(stat, false);
+	}
+
+	public ChunkStatsComparator(Stat stat, boolean isReverse) {
+		this.alternator = isReverse ? -1 : 1;
+		this.stat = stat;
+	}
+
+	@Override
+	public int compare(Entry<T, ChunkStats> entryA, Entry<T, ChunkStats> entryB) {
+		switch (stat) {
+			case ENTITIES:
+				return (int)(entryA.getValue().entityCount - entryB.getValue().entityCount) * alternator;
+			default:
+				return (int)(entryA.getValue().tickTime.getTotal() - entryB.getValue().tickTime.getTotal()) * alternator;
+			
+		}
+	}
+}
