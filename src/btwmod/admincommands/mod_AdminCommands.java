ca6299198109028ff6a3d35deae4c4f8de11bb85
Andre Mekkawi <github@andremekkawi.com>
2012-10-25 18:49:59 -0400

    Added basic AFK tracking to Admin Commands.

--- src/btwmod/admincommands/mod_AdminCommands.java
+++ src/btwmod/admincommands/mod_AdminCommands.java
@@ -1,10 +1,26 @@
 package btwmod.admincommands;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.src.EntityPlayerMP;
+import net.minecraft.src.Packet;
+import net.minecraft.src.Packet102WindowClick;
+import net.minecraft.src.Packet12PlayerLook;
+import net.minecraft.src.Packet13PlayerLookMove;
+import net.minecraft.src.Packet14BlockDig;
+import net.minecraft.src.Packet15Place;
+import net.minecraft.src.Packet3Chat;
 import btwmods.CommandsAPI;
 import btwmods.IMod;
+import btwmods.NetworkAPI;
 import btwmods.io.Settings;
+import btwmods.network.IPacketListener;
+import btwmods.network.PacketEvent;
 
-public class mod_AdminCommands implements IMod {
+public class mod_AdminCommands implements IMod, IPacketListener {
+	
+	private final Map<EntityPlayerMP, Long> lastPlayerAction = new HashMap<EntityPlayerMP, Long>();
 	
 	private WhoCommand whoCommand;
 	private DumpTrackedCommand dumpTrackedCommand;
@@ -16,13 +32,34 @@ public class mod_AdminCommands implements IMod {
 
 	@Override
 	public void init(Settings settings) throws Exception {
+		NetworkAPI.addListener(this);
 		CommandsAPI.registerCommand(whoCommand = new WhoCommand(), this);
 		CommandsAPI.registerCommand(dumpTrackedCommand = new DumpTrackedCommand(), this);
 	}
 
 	@Override
 	public void unload() throws Exception {
+		NetworkAPI.removeListener(this);
 		CommandsAPI.unregisterCommand(whoCommand);
 		CommandsAPI.unregisterCommand(dumpTrackedCommand);
+	}
+
+	@Override
+	public IMod getMod() {
+		return this;
+	}
+
+	@Override
+	public void packetAction(PacketEvent event) {
+		Packet packet = event.getPacket();
+		if (packet instanceof Packet12PlayerLook
+				|| packet instanceof Packet13PlayerLookMove
+				|| packet instanceof Packet102WindowClick
+				|| packet instanceof Packet14BlockDig
+				|| packet instanceof Packet15Place
+				|| packet instanceof Packet3Chat) {
+			
+			lastPlayerAction.put(event.getPlayer(), new Long(System.currentTimeMillis()));
+		}
 	}
 }
