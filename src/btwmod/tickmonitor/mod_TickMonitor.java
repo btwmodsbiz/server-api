36e794a9b6e2c1a6d6eb168de211db2ae0577571
Andre Mekkawi <github@andremekkawi.com>
2012-10-24 21:54:19 -0400

    Changed (Refactored) code in the Tick Monitor mod, and made the html and json output only happen if their settings are set.

--- src/btwmod/tickmonitor/mod_TickMonitor.java
+++ src/btwmod/tickmonitor/mod_TickMonitor.java
@@ -39,8 +39,8 @@ public class mod_TickMonitor implements IMod, IStatsListener, INetworkListener, 
 	private static int topNumber = 20;
 	private static boolean includeHistory = false;
 	private static String publicLink = null;
-	private static File htmlFile = new File(new File("."), "stats.html");
-	private static File jsonFile = new File(new File("."), "stats.txt");
+	private static File htmlFile = null; //new File(new File("."), "stats.html");
+	private static File jsonFile = null; //new File(new File("."), "stats.txt");
     private static long tooLongWarningTime = 1000;
     private static long reportingDelay = 1000;
 	
@@ -146,7 +146,6 @@ public class mod_TickMonitor implements IMod, IStatsListener, INetworkListener, 
 	public void statsAction(StatsEvent event) {
 		long currentTime = System.currentTimeMillis();
 		long startNano = System.nanoTime();
-		boolean detailedMeasurementsEnabled = StatsAPI.detailedMeasurementsEnabled;
 		
 		if (lastStatsTime == 0) {
 			lastStatsTime = System.currentTimeMillis();
@@ -159,136 +158,40 @@ public class mod_TickMonitor implements IMod, IStatsListener, INetworkListener, 
 			long timeSinceLastTick = currentTime - event.serverStats.lastTickEnd;
 			ticksPerSecond.record((long)((double)numTicks / (double)timeElapsed * 100000D));
 
-			long jsonTime = System.nanoTime();
-			JsonObject jsonObj = new JsonObject();
-			jsonObj.addProperty("tickCounter", event.tickCounter);
-			jsonObj.addProperty("timeSinceLastTick", timeSinceLastTick);
-			jsonObj.addProperty("ticksSinceLastStatsAction", numTicks);
-			jsonObj.addProperty("time", BasicFormatter.dateFormat.format(new Date(currentTime)));
-			jsonObj.addProperty("detailedMeasurements", StatsAPI.detailedMeasurementsEnabled);
-			
-			jsonObj.add("statsActionTime", gson.toJsonTree(statsActionTime));
-			jsonObj.add("statsActionIOTime", gson.toJsonTree(statsActionIOTime));
-			jsonObj.add("ticksPerSecondArray", gson.toJsonTree(ticksPerSecond));
-			
-			jsonObj.addProperty("jsonTime", "_____JSONTIME_____");
-			
-			JsonObject serverStats = (JsonObject)gson.toJsonTree(event.serverStats);
-			for (Map.Entry<String, JsonElement> entry : serverStats.entrySet()) {
-				jsonObj.add(entry.getKey(), entry.getValue());
-			}
-			
-			jsonObj.add("worlds", gson.toJsonTree(event.worldStats));
-			
-			String json = gson.toJson(jsonObj).replace("_____JSONTIME_____", Util.DECIMAL_FORMAT_3.format((jsonTime = System.nanoTime() - jsonTime) * 1.0E-6D));
-			
 			// Debugging loop to ramp up CPU usage by the thread.
 			//for (int i = 0; i < 20000; i++) new String(new char[10000]).replace('\0', 'a');
 			
-			StringBuilder html = new StringBuilder("<html><head><title>Minecraft Server Stats</title><meta http-equiv=\"refresh\" content=\"2\"></head><body><h1>Minecraft Server Stats</h1>");
+			String html = null;
+			if (htmlFile != null)
+				html = createHtml(event, timeSinceLastTick);
 			
-			html.append("<table border=\"0\"><tbody>"); 
-			
-			html.append("<tr><th align=\"right\">Updated:<th><td>").append(BasicFormatter.dateFormat.format(new Date())).append("</td></tr>");
-			
-			if (event.serverStats.lastTickEnd >= 0) {
-				html.append("<tr><th align=\"right\">Last Tick:<th><td>").append(timeSinceLastTick >= 1000 ? "Over " + (timeSinceLastTick / 1000) + " seconds" : timeSinceLastTick + "ms").append(" ago.</td></tr>");
-			}
-			
-			html.append("<tr><th align=\"right\">Average StatsAPI Thread Time:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.statsThreadTime.getAverage() * 1.0E-6D)).append(" ms</td></tr>");
-			html.append("<tr><th align=\"right\">Average StatsAPI Polled:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.statsThreadQueueCount.getAverage())).append("</td></tr>");
-			
-			html.append("<tr><th align=\"right\">Average Tick Monitor Time:<th><td>");
-			if (statsActionTime.getTick() == 0)
-				html.append("...");
-			else
-				html.append(Util.DECIMAL_FORMAT_3.format(statsActionTime.getAverage() * 1.0E-6D)).append("ms (" + (int)(statsActionIOTime.getAverage() * 100 / statsActionTime.getAverage()) + "% IO)");
-			html.append("</td></tr>");
-			
-			html.append("<tr><td colspan=\"2\" style=\"height: 16px\"></td></tr>");
-			
-			html.append("<tr><th align=\"right\">Tick Num:<th><td>").append(event.tickCounter);
-			if (lastTickCounter >= 0) html.append(" (~").append(Util.DECIMAL_FORMAT_3.format(ticksPerSecond.getAverage() / 100D)).append("/sec)");
-			html.append("</td></tr>");
-			
-			html.append("<tr><th align=\"right\">Average Full Tick:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.tickTime.getAverage() * 1.0E-6D)).append(" ms</td></tr>");
-			
-			html.append("<tr><td colspan=\"2\" style=\"height: 16px\"></td></tr>");
-			
-			double worldsTotal = 0;
-			for (int i = 0; i < event.worldStats.length; i++) {
-				
-				worldsTotal += event.worldStats[i].worldTickTime.getAverage();
-				
-				html.append("<tr><th align=\"right\">World ").append(i).append(" Averages:<th><td>")
-					.append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].worldTickTime.getAverage() * 1.0E-6D) + "ms");
-				
-				if (detailedMeasurementsEnabled)
-					html.append(" (")
-						.append("E: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].entities.getAverage() * 1.0E-6D)).append("ms")
-						.append(" + M: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].mobSpawning.getAverage() * 1.0E-6D)).append("ms")
-						.append(" + B: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].blockTick.getAverage() * 1.0E-6D)).append("ms")
-						.append(" + W: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].weather.getAverage() * 1.0E-6D)).append("ms")
-						.append(" + T: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].timeSync.getAverage() * 1.0E-6D)).append("ms")
-						.append(" + CS: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].buildActiveChunkSet.getAverage() * 1.0E-6D)).append("ms")
-						.append(" + L: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].checkPlayerLight.getAverage() * 1.0E-6D)).append("ms")
-						.append(")");
-				
-				html.append("</td></tr>");
-				
-				html.append("<tr><th align=\"right\">&nbsp;<th><td>")
-					.append("Chunks: ")
-					.append((int)event.worldStats[i].loadedChunks.getLatest()).append(" loaded, ")
-					.append(event.worldStats[i].id2ChunkMap).append(" cached, ")
-					.append((int)event.worldStats[i].droppedChunksSet.getAverage()).append(" dropped");
-				
-				if (detailedMeasurementsEnabled)
-					html.append(" | ").append((int)event.worldStats[i].measurementQueue.getAverage()).append(" measurements per tick");
-				
-				html.append("</td></tr>");
-			}
-
-			html.append("<tr><th align=\"right\">Worlds Total:<th><td>").append(Util.DECIMAL_FORMAT_3.format(worldsTotal * 1.0E-6D)).append(" ms (" + (int)(worldsTotal / event.serverStats.tickTime.getAverage() * 100) + "% of full tick)</td></tr>");
-			
-			html.append("<tr><td colspan=\"2\" style=\"height: 16px\"></td></tr>");
-			
-			html.append("<tr><th align=\"right\">Average Received Packet Count:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.receivedPacketCount.getAverage())).append("</td></tr>");
-			html.append("<tr><th align=\"right\">Average Sent Packet Count:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.sentPacketCount.getAverage())).append("</td></tr>");
-			
-			html.append("<tr><td colspan=\"2\" style=\"height: 16px\"></td></tr>");
-
-			html.append("<tr><th align=\"right\">Average Received Packet Size:<th><td>").append((int)event.serverStats.receivedPacketSize.getAverage()).append(" bytes</td></tr>");
-			html.append("<tr><th align=\"right\">Average Sent Packet Size:<th><td>").append((int)event.serverStats.sentPacketSize.getAverage()).append(" bytes</td></tr>");
-			
-			html.append("</tbody></table>");
-			
-			if (detailedMeasurementsEnabled) {
-				for (int i = 0; i < event.worldStats.length; i++) {
-					outputWorldDetails(event, html, i);
-				}
-			}
-	
-			html.append("</body></html>");
+			String json = null;
+			if (jsonFile != null)
+				json = createJson(event, timeSinceLastTick, numTicks, currentTime);
 
 			long startWriteTime = System.currentTimeMillis();
 			long startWriteNano = System.nanoTime();
 			
-			try {
-				FileWriter htmlWriter = new FileWriter(htmlFile);
-				htmlWriter.write(html.toString());
-				htmlWriter.close();
-			}
-			catch (Throwable e) {
-				ModLoader.outputError(getName() + " failed to write to " + htmlFile.getPath() + ": " + e.getMessage());
+			if (htmlFile != null) {
+				try {
+					FileWriter htmlWriter = new FileWriter(htmlFile);
+					htmlWriter.write(html);
+					htmlWriter.close();
+				}
+				catch (Throwable e) {
+					ModLoader.outputError(getName() + " failed to write to " + htmlFile.getPath() + ": " + e.getMessage());
+				}
 			}
 			
-			try {
-				FileWriter jsonWriter = new FileWriter(jsonFile);
-				jsonWriter.write(json);
-				jsonWriter.close();
-			}
-			catch (Throwable e) {
-				ModLoader.outputError(getName() + " failed to write to " + jsonFile.getPath() + ": " + e.getMessage());
+			if (jsonFile != null) {
+				try {
+					FileWriter jsonWriter = new FileWriter(jsonFile);
+					jsonWriter.write(json);
+					jsonWriter.close();
+				}
+				catch (Throwable e) {
+					ModLoader.outputError(getName() + " failed to write to " + jsonFile.getPath() + ": " + e.getMessage());
+				}
 			}
 			
 			long endNano = System.nanoTime();
@@ -302,6 +205,120 @@ public class mod_TickMonitor implements IMod, IStatsListener, INetworkListener, 
 			lastTickCounter = event.tickCounter;
 			lastStatsTime = System.currentTimeMillis();
 		}
+	}
+	
+	private String createJson(StatsEvent event, long timeSinceLastTick, int numTicks, long currentTime) {
+		long jsonTime = System.nanoTime();
+		JsonObject jsonObj = new JsonObject();
+		jsonObj.addProperty("tickCounter", event.tickCounter);
+		jsonObj.addProperty("timeSinceLastTick", timeSinceLastTick);
+		jsonObj.addProperty("ticksSinceLastStatsAction", numTicks);
+		jsonObj.addProperty("time", BasicFormatter.dateFormat.format(new Date(currentTime)));
+		jsonObj.addProperty("detailedMeasurements", StatsAPI.detailedMeasurementsEnabled);
+		
+		jsonObj.add("statsActionTime", gson.toJsonTree(statsActionTime));
+		jsonObj.add("statsActionIOTime", gson.toJsonTree(statsActionIOTime));
+		jsonObj.add("ticksPerSecondArray", gson.toJsonTree(ticksPerSecond));
+		
+		jsonObj.addProperty("jsonTime", "_____JSONTIME_____");
+		
+		JsonObject serverStats = (JsonObject)gson.toJsonTree(event.serverStats);
+		for (Map.Entry<String, JsonElement> entry : serverStats.entrySet()) {
+			jsonObj.add(entry.getKey(), entry.getValue());
+		}
+		
+		jsonObj.add("worlds", gson.toJsonTree(event.worldStats));
+		
+		return gson.toJson(jsonObj).replace("_____JSONTIME_____", Util.DECIMAL_FORMAT_3.format((jsonTime = System.nanoTime() - jsonTime) * 1.0E-6D));
+	}
+	
+	private String createHtml(StatsEvent event, long timeSinceLastTick) {
+		StringBuilder html = new StringBuilder("<html><head><title>Minecraft Server Stats</title><meta http-equiv=\"refresh\" content=\"2\"></head><body><h1>Minecraft Server Stats</h1>");
+		
+		html.append("<table border=\"0\"><tbody>"); 
+		
+		html.append("<tr><th align=\"right\">Updated:<th><td>").append(BasicFormatter.dateFormat.format(new Date())).append("</td></tr>");
+		
+		if (event.serverStats.lastTickEnd >= 0) {
+			html.append("<tr><th align=\"right\">Last Tick:<th><td>").append(timeSinceLastTick >= 1000 ? "Over " + (timeSinceLastTick / 1000) + " seconds" : timeSinceLastTick + "ms").append(" ago.</td></tr>");
+		}
+		
+		html.append("<tr><th align=\"right\">Average StatsAPI Thread Time:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.statsThreadTime.getAverage() * 1.0E-6D)).append(" ms</td></tr>");
+		html.append("<tr><th align=\"right\">Average StatsAPI Polled:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.statsThreadQueueCount.getAverage())).append("</td></tr>");
+		
+		html.append("<tr><th align=\"right\">Average Tick Monitor Time:<th><td>");
+		if (statsActionTime.getTick() == 0)
+			html.append("...");
+		else
+			html.append(Util.DECIMAL_FORMAT_3.format(statsActionTime.getAverage() * 1.0E-6D)).append("ms (" + (int)(statsActionIOTime.getAverage() * 100 / statsActionTime.getAverage()) + "% IO)");
+		html.append("</td></tr>");
+		
+		html.append("<tr><td colspan=\"2\" style=\"height: 16px\"></td></tr>");
+		
+		html.append("<tr><th align=\"right\">Tick Num:<th><td>").append(event.tickCounter);
+		if (lastTickCounter >= 0) html.append(" (~").append(Util.DECIMAL_FORMAT_3.format(ticksPerSecond.getAverage() / 100D)).append("/sec)");
+		html.append("</td></tr>");
+		
+		html.append("<tr><th align=\"right\">Average Full Tick:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.tickTime.getAverage() * 1.0E-6D)).append(" ms</td></tr>");
+		
+		html.append("<tr><td colspan=\"2\" style=\"height: 16px\"></td></tr>");
+		
+		double worldsTotal = 0;
+		for (int i = 0; i < event.worldStats.length; i++) {
+			
+			worldsTotal += event.worldStats[i].worldTickTime.getAverage();
+			
+			html.append("<tr><th align=\"right\">World ").append(i).append(" Averages:<th><td>")
+				.append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].worldTickTime.getAverage() * 1.0E-6D) + "ms");
+			
+			if (StatsAPI.detailedMeasurementsEnabled)
+				html.append(" (")
+					.append("E: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].entities.getAverage() * 1.0E-6D)).append("ms")
+					.append(" + M: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].mobSpawning.getAverage() * 1.0E-6D)).append("ms")
+					.append(" + B: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].blockTick.getAverage() * 1.0E-6D)).append("ms")
+					.append(" + W: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].weather.getAverage() * 1.0E-6D)).append("ms")
+					.append(" + T: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].timeSync.getAverage() * 1.0E-6D)).append("ms")
+					.append(" + CS: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].buildActiveChunkSet.getAverage() * 1.0E-6D)).append("ms")
+					.append(" + L: ").append(Util.DECIMAL_FORMAT_3.format(event.worldStats[i].checkPlayerLight.getAverage() * 1.0E-6D)).append("ms")
+					.append(")");
+			
+			html.append("</td></tr>");
+			
+			html.append("<tr><th align=\"right\">&nbsp;<th><td>")
+				.append("Chunks: ")
+				.append((int)event.worldStats[i].loadedChunks.getLatest()).append(" loaded, ")
+				.append(event.worldStats[i].id2ChunkMap).append(" cached, ")
+				.append((int)event.worldStats[i].droppedChunksSet.getAverage()).append(" dropped");
+			
+			if (StatsAPI.detailedMeasurementsEnabled)
+				html.append(" | ").append((int)event.worldStats[i].measurementQueue.getAverage()).append(" measurements per tick");
+			
+			html.append("</td></tr>");
+		}
+
+		html.append("<tr><th align=\"right\">Worlds Total:<th><td>").append(Util.DECIMAL_FORMAT_3.format(worldsTotal * 1.0E-6D)).append(" ms (" + (int)(worldsTotal / event.serverStats.tickTime.getAverage() * 100) + "% of full tick)</td></tr>");
+		
+		html.append("<tr><td colspan=\"2\" style=\"height: 16px\"></td></tr>");
+		
+		html.append("<tr><th align=\"right\">Average Received Packet Count:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.receivedPacketCount.getAverage())).append("</td></tr>");
+		html.append("<tr><th align=\"right\">Average Sent Packet Count:<th><td>").append(Util.DECIMAL_FORMAT_3.format(event.serverStats.sentPacketCount.getAverage())).append("</td></tr>");
+		
+		html.append("<tr><td colspan=\"2\" style=\"height: 16px\"></td></tr>");
+
+		html.append("<tr><th align=\"right\">Average Received Packet Size:<th><td>").append((int)event.serverStats.receivedPacketSize.getAverage()).append(" bytes</td></tr>");
+		html.append("<tr><th align=\"right\">Average Sent Packet Size:<th><td>").append((int)event.serverStats.sentPacketSize.getAverage()).append(" bytes</td></tr>");
+		
+		html.append("</tbody></table>");
+		
+		if (StatsAPI.detailedMeasurementsEnabled) {
+			for (int i = 0; i < event.worldStats.length; i++) {
+				outputWorldDetails(event, html, i);
+			}
+		}
+
+		html.append("</body></html>");
+		
+		return html.toString();
 	}
 	
 	private void outputWorldDetails(StatsEvent event, StringBuilder html, int world) {
