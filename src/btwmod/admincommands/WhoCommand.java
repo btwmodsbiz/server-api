b96abe79a1ceedd9021080cc6d7b19d0847516ca
Andre Mekkawi <github@andremekkawi.com>
2012-11-02 18:26:32 -0400

    Changed the /who command in Admin Commands to allow normal players to see who is AFK.

--- src/btwmod/admincommands/WhoCommand.java
+++ src/btwmod/admincommands/WhoCommand.java
@@ -7,13 +7,16 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.CommandBase;
 import net.minecraft.src.EntityPlayerMP;
 import net.minecraft.src.ICommandSender;
+import net.minecraft.src.WrongUsageException;
 
 public class WhoCommand extends CommandBase {
 	
 	private final mod_AdminCommands mod;
+	private final MinecraftServer mcServer;
 	
 	public WhoCommand(mod_AdminCommands mod) {
 		this.mod = mod;
+		mcServer = MinecraftServer.getServer();
 	}
 
 	@Override
@@ -22,8 +25,35 @@ public class WhoCommand extends CommandBase {
 	}
 
 	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender sender) {
+		return true;
+	}
+
+	@Override
 	public void processCommand(ICommandSender sender, String[] args) {
-		if (args.length == 0) {
+		if (!isFullUsageAllowed(sender)) {
+			if (args.length != 0)
+				throw new WrongUsageException(getCommandUsage(sender), new Object[0]);
+
+			List players = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+			StringBuilder sb = new StringBuilder();
+			
+			Iterator playerIterator = players.iterator();
+			while (playerIterator.hasNext()) {
+				EntityPlayerMP player = (EntityPlayerMP)playerIterator.next();
+				if (sb.length() > 0)
+					sb.append(", ");
+				
+				sb.append(player.username);
+
+				long seconds = mod.getTimeSinceLastPlayerAction(player);
+				if (seconds >= mod.getSecondsForAFK())
+					sb.append(" (AFK ").append(formatSeconds(seconds)).append(")");
+				
+				sender.sendChatToPlayer("Players: " + sb.toString());
+			}
+		}
+		else if (args.length == 0) {
 			List players = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
 			
 			if (players.size() == 0) {
@@ -54,14 +84,37 @@ public class WhoCommand extends CommandBase {
 	private String getPlayerResult(EntityPlayerMP player) {
 		long seconds = mod.getTimeSinceLastPlayerAction(player);
 		return player.username + " in " + player.worldObj.provider.getDimensionName() + " at " + (long)player.posX + " " + (long)player.posY + " "
-				+ (long)player.posZ + (seconds >= mod.getSecondsForAFK() ? " (AFK " + seconds + " seconds)" : "");
+				+ (long)player.posZ + (seconds >= mod.getSecondsForAFK() ? " (AFK " + formatSeconds(seconds) + ")" : "");
 	}
 
+	@Override
 	public String getCommandUsage(ICommandSender sender) {
-		return "/" + getCommandName() + " [<playername> ...]";
+		return "/" + getCommandName() + (isFullUsageAllowed(sender) ? " [<playername> ...]" : "");
 	}
 
-	public List addTabCompletionOptions(ICommandSender par1ICommandSender, String[] par2ArrayOfStr) {
-		return getListOfStringsMatchingLastWord(par2ArrayOfStr, MinecraftServer.getServer().getAllUsernames());
+	@Override
+	public List addTabCompletionOptions(ICommandSender sender, String[] args) {
+		if (isFullUsageAllowed(sender))
+			return getListOfStringsMatchingLastWord(args, MinecraftServer.getServer().getAllUsernames());
+		else
+			return super.addTabCompletionOptions(sender, args);
+	}
+	
+	private boolean isFullUsageAllowed(ICommandSender sender) {
+		return sender instanceof EntityPlayerMP
+				? mcServer.getConfigurationManager().areCommandsAllowed(((EntityPlayerMP)sender).username)
+				: true;
+	}
+	
+	private static String formatSeconds(long seconds) {
+		if (seconds < 60L) {
+			return seconds + " seconds";
+		}
+		else if (seconds < 3600L) {
+			return seconds / 60L + "min";
+		}
+		else {
+			return String.format("%dh%02dm", seconds / 3600L, seconds % 3600L / 60L);
+		}
 	}
 }
