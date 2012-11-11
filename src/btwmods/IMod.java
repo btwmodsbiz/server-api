39a077fd6c496b593f78e2f6ffb5e309c2edeba8
Andre Mekkawi <github@andremekkawi.com>
2012-11-11 18:39:15 -0500

    Changed getName() in IMod to not declare thrown Exceptions.

--- src/btwmods/IMod.java
+++ src/btwmods/IMod.java
@@ -7,9 +7,8 @@ public interface IMod {
 	/**
 	 * Human-readable name of the mod that will display in the logs.
 	 * @return the name of the mod.
-	 * @throws Exception 
 	 */
-	public String getName() throws Exception;
+	public String getName();
 	
 	public void init(Settings settings) throws Exception;
 	
