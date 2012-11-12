5730e5e157a9184e55a6277cce85e11400f8e964
Andre Mekkawi <github@andremekkawi.com>
2012-11-12 03:27:14 -0500

    Major improvements to Settings.

--- src/btwmods/io/Settings.java
+++ src/btwmods/io/Settings.java
@@ -1,52 +1,95 @@
 package btwmods.io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Settings {
 	
 	public static Settings readSettings(File file) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		Map<String, String> settings = new LinkedHashMap<String, String>();
+		FileReader reader = null;
+		try {
+			reader = new FileReader(file);
+			return readSettings(new BufferedReader(reader));
+		}
+		finally {
+			try {
+				if (reader != null)
+					reader.close();
+			}
+			catch (IOException e) { }
+		}
+	}
+	
+	public static Settings readSettings(String settings) {
+		try {
+			return readSettings(new BufferedReader(new StringReader(settings)));
+		}
+		catch (IOException e) {
+			return new Settings();
+		}
+	}
+	
+	public static Settings readSettings(BufferedReader reader) throws IOException {
+		Settings settings = new Settings();
 
-		String section = "";
+		String section = null;
 		String line;
 		int equalsIndex;
+		
 		while ((line = reader.readLine()) != null) {
 			// Trim leading whitespace.
 			line = line.replaceAll("^[ \t]+", "");
 			
 			if (!line.startsWith("#")) {
-				if (line.startsWith("[") && line.trim().endsWith("]")) {
-					section = line.trim();
+				if (line.trim().startsWith("[") && line.trim().endsWith("]")) {
+					section = line.trim().substring(1, line.trim().length() - 2);
 				}
 				else if ((equalsIndex = line.indexOf('=')) >= 0) {
-					settings.put(section.toLowerCase() + line.substring(0, equalsIndex).toLowerCase(), line.substring(equalsIndex + 1));
+					settings.set(section, line.substring(0, equalsIndex), line.substring(equalsIndex + 1));
 				}
 			}
 		}
-		reader.close();
 		
-		return new Settings(settings);
+		return settings;
 	}
 	
-	public final Map<String, String> settings;
+	private final Map<String, String> settings;
+	private final Map<String, String> lowercaseLookup;
 	
 	public Settings() {
-		this(new HashMap<String, String>());
+		this(new LinkedHashMap<String, String>());
 	}
 	
 	public Settings(Map<String, String> settings) {
-		this.settings = Collections.unmodifiableMap(settings);
+		this.settings = settings;
+		
+		// Create lowercase versions of the keys for lookup.
+		lowercaseLookup = new LinkedHashMap<String, String>();
+		for (Map.Entry<String, String> entry : settings.entrySet()) {
+			lowercaseLookup.put(entry.getKey().toLowerCase(), entry.getKey());
+		}
+	}
+	
+	public Map getSettings() {
+		return Collections.unmodifiableMap(settings);
 	}
 	
 	public boolean isBoolean(String key) {
+		return isBoolean(null, key);
+	}
+	
+	public boolean isBoolean(String section, String key) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
 		if (hasKey(key)) {
 			String setting = get(key).trim().toLowerCase();
 			return setting.equalsIgnoreCase("yes") || setting.equalsIgnoreCase("true")|| setting.equalsIgnoreCase("1") || setting.equalsIgnoreCase("on")
@@ -56,36 +99,172 @@ public class Settings {
 	}
 	
 	public boolean getBoolean(String key) {
+		return getBoolean(null, key);
+	}
+	
+	public boolean getBoolean(String section, String key) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
 		if (!isBoolean(key)) throw new IllegalArgumentException("setting is not a valid boolean. check with isBoolean() first");
 		String setting = get(key).trim().toLowerCase();
 		return setting.equalsIgnoreCase("yes") || setting.equalsIgnoreCase("true") || setting.equalsIgnoreCase("1") || setting.equalsIgnoreCase("on");
 	}
 	
 	public boolean isInt(String key) {
+		return isInt(null, key);
+	}
+	
+	public boolean isInt(String section, String key) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
 		try { return hasKey(key) && Integer.valueOf(get(key)) != null; }
 		catch (NumberFormatException e) { return false; }
 	}
 	
 	public int getInt(String key) {
+		return getInt(null, key);
+	}
+	
+	public int getInt(String section, String key) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
 		if (!isInt(key)) throw new IllegalArgumentException("setting is not a valid Integer. check with isInt() first");
 		return Integer.parseInt(get(key));
 	}
 	
 	public boolean isLong(String key) {
+		return isLong(null, key);
+	}
+	
+	public boolean isLong(String section, String key) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
 		try { return hasKey(key) && Long.valueOf(get(key)) != null; }
 		catch (NumberFormatException e) { return false; }
 	}
 	
 	public long getLong(String key) {
+		return getLong(null, key);
+	}
+	
+	public long getLong(String section, String key) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
 		if (!isLong(key)) throw new IllegalArgumentException("setting is not a valid Long. check with isLong() first");
 		return Long.parseLong(get(key));
 	}
 	
 	public boolean hasKey(String key) {
-		return settings.containsKey(key.toLowerCase());
+		return hasKey(null, key);
+	}
+	
+	public boolean hasKey(String section, String key) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
+		return lowercaseLookup.containsKey(key.toLowerCase());
 	}
 	
 	public String get(String key) {
-		return settings.get(key.toLowerCase());
+		return get(null, key);
+	}
+	
+	public String get(String section, String key) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
+		String lookupKey = lowercaseLookup.get(key.toLowerCase());
+		return lookupKey == null ? null : settings.get(lookupKey);
+	}
+	
+	public void setBoolean(String key, boolean value) {
+		set(key, value ? "yes" : "no");
+	}
+	
+	public void setInt(String key, int value) {
+		set(key, Integer.toString(value));
+	}
+	
+	public void setLong(String key, long value) {
+		set(key, Long.toString(value));
+	}
+	
+	public void setBoolean(String section, String key, boolean value) {
+		set(section, key, value ? "yes" : "no");
+	}
+	
+	public void setInt(String section, String key, int value) {
+		set(section, key, Integer.toString(value));
+	}
+	
+	public void setLong(String section, String key, long value) {
+		set(section, key, Long.toString(value));
+	}
+	
+	public void set(String key, String value) {
+		set(null, key, value);
+	}
+	
+	public void set(String section, String key, String value) {
+		if (section != null)
+			key = "[" + section + "]" + key;
+		
+		String lookupKey = lowercaseLookup.get(key.toLowerCase());
+		if (lookupKey == null)
+			lowercaseLookup.put(lookupKey = key.toLowerCase(), key);
+		
+		settings.put(key, value);
+	}
+	
+	public void writeSettings(File file) throws IOException {
+		writeSettings(new BufferedWriter(new FileWriter(file)));
+	}
+	
+	public void writeSettings(BufferedWriter writer) throws IOException {
+		Map<String, Map<String, String>> sections = new LinkedHashMap<String, Map<String, String>>();
+		
+		for (Map.Entry<String, String> entry : settings.entrySet()) {
+			String section = null;
+			String key = entry.getKey();
+			
+			if (key.startsWith("[")) {
+				int endSectionIndex = key.indexOf(']', 1);
+				section = key.substring(1, endSectionIndex);
+				key = key.substring(endSectionIndex + 1);
+			}
+			
+			if (!sections.containsKey(section)) {
+				sections.put(section, new LinkedHashMap<String, String>());
+			}
+			
+			Map<String, String> sectionMap = sections.get(section);
+			sectionMap.put(key, entry.getValue());
+		}
+		
+		if (sections.containsKey(null)) {
+			for (Map.Entry<String, String> entry : sections.get(null).entrySet()) {
+				writer.write(entry.getKey() + "=" + entry.getValue());
+				writer.newLine();
+			}
+			
+			writer.newLine();
+		}
+		
+		for (Map.Entry<String, Map<String, String>> sectionEntry : sections.entrySet()) {
+			writer.write("[" + sectionEntry.getKey() + "]");
+			writer.newLine();
+			
+			for (Map.Entry<String, String> pairEntry : sectionEntry.getValue().entrySet()) {
+				writer.write("\t" + pairEntry.getKey() + "=" + pairEntry.getValue());
+				writer.newLine();
+			}
+			
+			writer.newLine();
+		}
 	}
 }
