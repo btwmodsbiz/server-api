137523803dd094fcb3377f25fa8b02b699d6f45e
Andre Mekkawi <github@andremekkawi.com>
2012-11-17 12:17:17 -0500

    Added the data field to Area's hashCode.

--- src/btwmods/util/Area.java
+++ src/btwmods/util/Area.java
@@ -23,6 +23,7 @@ public class Area<Type> {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
 		result = prime * result + x1;
 		result = prime * result + x2;
 		result = prime * result + z1;
