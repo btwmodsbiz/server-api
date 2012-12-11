56a9da1f35fc6dfd2b11123f9bedacf392e3a771
Andre Mekkawi <github@andremekkawi.com>
2012-12-11 04:55:24 -0500

    Added yaw and pitch to SpawnPosition.

--- src/btwmods/player/SpawnPosition.java
+++ src/btwmods/player/SpawnPosition.java
@@ -1,15 +1,34 @@
 package btwmods.player;
 
 public class SpawnPosition {
+	
 	public final int dimension;
+	
 	public final int x;
 	public final int y;
 	public final int z;
 	
+	public final float pitch;
+	public final float yaw;
+	
+	public SpawnPosition(int x, int y, int z) {
+		this(0, x, y, z, 0.0F, 0.0F);
+	}
+	
+	public SpawnPosition(int x, int y, int z, float yaw, float pitch) {
+		this(0, x, y, z, yaw, pitch);
+	}
+	
 	public SpawnPosition(int dimension, int x, int y, int z) {
+		this(dimension, x, y, z, 0.0F, 0.0F);
+	}
+	
+	public SpawnPosition(int dimension, int x, int y, int z, float yaw, float pitch) {
 		this.dimension = dimension;
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		this.yaw = yaw;
+		this.pitch = pitch;
 	}
 }
