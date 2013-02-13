401bfa2eeb369dc110a35ba56c278c36f28780b4
Andre Mekkawi <github@andremekkawi.com>
2013-02-12 19:54:02 -0500

    Added onDoQuietCommand method to CommandsAPI so vMC commands can execute quietly when executed via the console.

--- src/btwmods/CommandsAPI.java
+++ src/btwmods/CommandsAPI.java
@@ -1,11 +1,15 @@
 package btwmods;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.CommandHandler;
+import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.ICommand;
+import net.minecraft.src.ICommandSender;
 import btwmods.commands.CommandWrapper;
 
 public class CommandsAPI {
@@ -28,5 +32,30 @@ public class CommandsAPI {
 		CommandWrapper wrapper = commandWrapperLookup.get(command);
 		if (wrapper != null)
 			((CommandHandler)MinecraftServer.getServer().getCommandManager()).unregisterCommand(wrapper);
+	}
+	
+	public static boolean onDoQuietCommand(ICommand command, ICommandSender sender, String[] args, boolean status) {
+		if (!(sender instanceof EntityPlayer) && args.length >= 1 && args[0].equalsIgnoreCase("quiet")) {
+			if (args.length == 2) {
+				try {
+					File statusFile = new File(args[1]);
+					if (statusFile.isFile() && statusFile.length() == 0) {
+						FileWriter writer = new FileWriter(statusFile);
+						writer.write(status ? "OK" : "FAIL");
+						writer.close();
+					}
+					else {
+						btwmods.ModLoader.outputError("Quiet-mode status file for /" + command.getCommandName() + " was not a file or already contained data.");
+					}
+				}
+				catch (Exception e) {
+					btwmods.ModLoader.outputError(e, "Failed to write to quiet-mode status file for /" + command.getCommandName() + ": " + e.getMessage());
+				}
+			}
+			
+			return false;
+		}
+		
+		return true;
 	}
 }
