bb35124f3ea46e37f38f0fd65f62631bb23c0572
Andre Mekkawi <github@andremekkawi.com>
2013-02-28 23:52:23 -0500

    Removed helper methods from StatPositionedClass.

--- src/btwmods/stats/measurements/StatPositionedClass.java
+++ src/btwmods/stats/measurements/StatPositionedClass.java
@@ -1,9 +1,6 @@
 package btwmods.stats.measurements;
 
 import btwmods.Stat;
-import net.minecraft.src.Entity;
-import net.minecraft.src.EntityTrackerEntry;
-import net.minecraft.src.TileEntity;
 import net.minecraft.src.World;
 
 public class StatPositionedClass extends StatPositioned {
@@ -19,17 +16,5 @@ public class StatPositionedClass extends StatPositioned {
 		super(stat, world, x, y, z);
 		this.clazz = clazz;
 		this.id = id;
-	}
-
-	public StatPositionedClass(Stat stat, World world, TileEntity tileEntity) {
-		this(stat, world, tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord, tileEntity.getClass());
-	}
-	
-	public StatPositionedClass(World world, Entity entity) {
-		this(Stat.ENTITY_UPDATE, world, entity.posX, entity.posY, entity.posZ, entity.getClass(), entity.entityId);
-	}
-	
-	public StatPositionedClass(World world, EntityTrackerEntry trackerEntry) {
-		this(Stat.UPDATE_TRACKED_ENTITY_PLAYER_LIST, world, trackerEntry.trackedEntity.posX, trackerEntry.trackedEntity.posY, trackerEntry.trackedEntity.posZ, trackerEntry.trackedEntity.getClass(), trackerEntry.trackedEntity.entityId);
 	}
 }
