5b2d386179b1308adc2cff8984df6a4ece21d8d8
Andre Mekkawi <github@andremekkawi.com>
2013-07-27 15:31:38 -0400

    Updated changelog and ModLoader for v4.891.2 release.

--- src/btwmods/ModLoader.java
+++ src/btwmods/ModLoader.java
@@ -36,7 +36,7 @@ public class ModLoader {
 	/**
 	 * Version label for BTWMods
 	 */
-	public static final String VERSION = "4.891.1 (vMC 1.5.2 BTW 4.891)";
+	public static final String VERSION = "4.891.2 (vMC 1.5.2 BTW 4.891 BoP 0.0.9)";
 	
 	/**
 	 * Location of settings and mods.
